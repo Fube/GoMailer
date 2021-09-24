e22b55168cdf1d6cc83ccf7710841f74b1a0b440
Fube <fubeitch@gmail.com>
2021-09-24 17:58:32 -0400

    Refined code coverage script

--- cover.sh
+++ cover.sh
@@ -1,1 +1,13 @@
-go test -v -coverpkg=./... -coverprofile=cover.out ./...&& go tool cover -html=cover.out -o cover.html
+act=$(go test -v -coverpkg=./... -coverprofile=cover.out ./...\
+| grep coverage\
+| awk 'NR==1 {print substr($2, 0, length($2)-1)}'\
+&& go tool cover -html=cover.out -o cover.html)
+
+cvg=${cvg:=90}
+
+if [ 1 -eq "$(echo "${act} < ${cvg}" | bc)" ]
+then
+  echo "Code coverage test failed"
+  echo "Expected minimum $cvg%, got $act%"
+  exit 1
+fi
