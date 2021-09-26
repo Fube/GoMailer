204d2507336497265195d4b3d2dc676ee7f8e6e2
Fube <fubeitch@gmail.com>
2021-09-25 22:47:13 -0400

    Added visual flag to cover.sh

--- cover.sh
+++ cover.sh
@@ -1,6 +1,10 @@
+go test -race -covermode=atomic -v -coverpkg=./... -coverprofile=cover.out ./...
+
 # Generate visual report
-go test -race -covermode=atomic -v -coverpkg=./... -coverprofile=cover.out ./...\
-&& go tool cover -html=cover.out -o cover.html
+if [[ "$1" == "visual" ]]
+then
+  go tool cover -html=cover.out -o cover.html
+fi
 
 # Get overall coverage percentage for coverage threshold testing
 act=$(go tool cover -func cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
